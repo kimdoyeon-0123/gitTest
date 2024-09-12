8ddaad14a57940ab458af4c61025a925cdb1e4ac
TJ <TJ@DESKTOP-8FSFFJK>
2024-09-12 17:04:56 +0900

    egit test

---
+++ src/egitTest/HelloWorld.java
@@ -0,0 +1,9 @@
+package egitTest;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("추가내용");
+	}
+}
